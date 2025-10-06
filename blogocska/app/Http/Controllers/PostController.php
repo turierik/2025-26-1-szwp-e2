4bf2d0beb089b53e5374f5098b4737d0607e4af9
Tűri Erik <54734700+turierik@users.noreply.github.com>
2025-10-06 19:23:06 +0200

    5. gyakorlat

--- blogocska/app/Http/Controllers/PostController.php
+++ blogocska/app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class PostController extends Controller
 {
@@ -16,5 +18,28 @@ class PostController extends Controller
 
     public function show(Post $post){
         return view('posts.show', ['post' => $post]);
+    }
+
+    public function create(){
+        return view('posts.create', [
+            'categories' => Category::all(),
+            'users' => User::all()
+        ]);
+    }
+
+    public function store(Request $request){
+        $validated = $request -> validate([
+            'title' => 'required|string',
+            'content' => 'required|string|min:10',
+            'author_id' => 'required|integer|exists:users,id',
+            'categories' => 'nullable|array',
+            'categories.*' => 'integer|distinct|exists:categories,id'
+        ], [
+            'content.min' => 'A tartalom legalább 10 karakter kell legyen!'
+        ]);
+        $validated['is_public'] = $request -> has('is_public');
+        $post = Post::create($validated);
+        $post -> categories() -> sync($validated['categories'] ?? []);
+        return redirect() -> route('posts.index');
     }
 }
